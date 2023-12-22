09d02aacc472fecbcf38e216a83db0a57fbe9195
Mohamed Samy <mohamedsabaawy@hotmail.com>
2023-12-22 10:44:11 +0200

    edit users role

--- resources/views/layouts/navigation-adminlte.blade.php
+++ resources/views/layouts/navigation-adminlte.blade.php
@@ -61,14 +61,14 @@
                 </p>
             </a>
         </li>
-        <li class="nav-item">
-            <a href="pages/gallery.html" class="nav-link">
-                <i class="nav-icon far fa-image"></i>
-                <p>
-                    Gallery
-                </p>
-            </a>
-        </li>
+{{--        <li class="nav-item">--}}
+{{--            <a href="pages/gallery.html" class="nav-link">--}}
+{{--                <i class="nav-icon far fa-image"></i>--}}
+{{--                <p>--}}
+{{--                    Gallery--}}
+{{--                </p>--}}
+{{--            </a>--}}
+{{--        </li>--}}
         <li class="nav-header">MISCELLANEOUS</li>
     </ul>
 </nav>
